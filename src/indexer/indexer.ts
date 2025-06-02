8535353dfd2866985c2d49f735698cd2285f9522
Juri Donvito <cirripide@MAC-5C30DE.station>
2025-06-02 16:51:58 +0200

    feat: Add ollama - with local LLM -.

---
+++ src/indexer/indexer.ts
@@ -0,0 +1,171 @@
+import dotenv from 'dotenv';
+import fs from 'fs/promises';
+import path from 'path';
+import {SingleBar} from "cli-progress";
+import {DocxLoader} from "@langchain/community/document_loaders/fs/docx";
+import {Document} from "@langchain/core/documents";
+import {TextLoader} from "langchain/document_loaders/fs/text";
+import {PDFLoader} from "@langchain/community/document_loaders/fs/pdf";
+import {DocumentLoader} from "@langchain/core/document_loaders/base";
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
+import {OpenAIEmbeddings} from "@langchain/openai";
+import {Pinecone} from "@pinecone-database/pinecone";
+import {PineconeStore} from "@langchain/pinecone";
+
+
+export default class Indexer {
+
+    filesExtensions: string[] = ['.docx', '.txt', '.pdf'];
+
+    constructor() {
+        dotenv.config();
+    }
+
+    public async crawlDocsPaths(): Promise<string[]> {
+
+        const docs: string[] = [];
+
+        console.log("Crawling Documents...");
+
+        const progressBar = new SingleBar({
+            format: "Documents Crawled: {value}",
+        });
+
+        progressBar.start(1000, 0);
+
+        if (!process.env['FOLDER_PATH']) {
+            console.error("No folder path found");
+            progressBar.stop();
+        }
+
+        await this.recursiveDocFind({directory: process.env['FOLDER_PATH'] as string, foundDocumentsPath: docs, progressBar});
+
+        progressBar.stop();
+        return docs;
+    }
+
+    public async loadDocuments(documentsPaths: string[]): Promise<Document[]> {
+
+        const progressBar = new SingleBar({});
+
+        console.log(`Starting document download. ${documentsPaths.length} total documents`);
+
+        progressBar.start(documentsPaths.length, 0);
+
+        const rawDocuments: Document[] = [];
+
+        let loadErrors = [];
+
+        for (const documentPath of documentsPaths) {
+            try {
+                const docs = await this.loadDocument(documentPath);
+                rawDocuments.push(...docs);
+            } catch (error) {
+                loadErrors.push(documentPath);
+            }
+            progressBar.increment();
+        }
+
+        progressBar.stop();
+
+        if (loadErrors.length) {
+            console.log(`Some documents may not have been loaded. Check for blank or corrupted documents. Paths: \n${loadErrors.join('\n')}`);
+        }
+
+        return rawDocuments;
+    }
+
+    public async chunkDocuments(rawDocuments: Document[]): Promise<Document[]> {
+        console.log('splitting documents...');
+
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 200
+        });
+
+        const documentChunks = await splitter.splitDocuments(rawDocuments);
+
+        console.log(`${rawDocuments.length} documents split into ${documentChunks.length} chunks`);
+
+        return documentChunks;
+    }
+
+    public async vectorizeChunks(chunks: Document[]) {
+
+        const embeddingLLM = new OpenAIEmbeddings({
+            model: 'text-embedding-3-small'
+        });
+
+        const pinecone = new Pinecone();
+
+        const pineconeIndexDataOperation = pinecone.index(process.env['PINECONE_INDEX'] as string);
+
+        console.log('Starting Vectorization...');
+        const progressBar = new SingleBar({});
+        progressBar.start(chunks.length, 0);
+
+        for (let i = 0; i < chunks.length; i = i + 100) {
+            const batch = chunks.slice(i, i + 100);
+
+            await PineconeStore.fromDocuments(batch, embeddingLLM, {pineconeIndex: pineconeIndexDataOperation as any});
+
+            progressBar.increment(batch.length);
+        }
+
+        progressBar.stop();
+        console.log('Vectorization completed and chunked stored in pinecone.');
+    }
+
+    public async index() {
+        const docsPaths = await this.crawlDocsPaths();
+
+        const docs = await this.loadDocuments(docsPaths);
+
+        const chunks = await this.chunkDocuments(docs);
+
+        await this.vectorizeChunks(chunks);
+    }
+
+    private async recursiveDocFind(config: { directory: string, foundDocumentsPath: string[], progressBar: SingleBar }): Promise<void> {
+        const elements = await fs.readdir(config.directory);
+
+        for (const element of elements) {
+            const fullPath = path.join(config.directory, element);
+            const state = await fs.stat(fullPath);
+
+            if (state.isDirectory()) {
+                await this.recursiveDocFind({
+                    directory: fullPath,
+                    foundDocumentsPath: config.foundDocumentsPath,
+                    progressBar: config.progressBar,
+                });
+            } else if (this.filesExtensions.includes(path.extname(fullPath))) {
+                config.progressBar.increment(1);
+                config.foundDocumentsPath.push(fullPath);
+            }
+        }
+    }
+
+    private async loadDocument(documentPath: string): Promise<Document[]> {
+
+        const fileType = path.extname(documentPath);
+
+        let loader: DocumentLoader;
+
+        switch (fileType) {
+            case '.docx':
+                loader = new DocxLoader(documentPath);
+                break;
+            case '.txt':
+                loader = new TextLoader(documentPath);
+                break;
+            case '.pdf':
+                loader = new PDFLoader(documentPath);
+                break;
+            default:
+                loader = new DocxLoader(documentPath);
+        }
+
+        return await loader.load();
+    }
+}
---
+++ src/rag.ts
@@ -0,0 +1,97 @@
+import {ChatPromptTemplate, MessagesPlaceholder} from "@langchain/core/prompts";
+import { Ollama } from "@langchain/ollama";
+import {StringOutputParser} from "@langchain/core/output_parsers";
+import {createRetriever} from "./retriever/retriever";
+import {RunnableSequence} from "@langchain/core/runnables";
+import {formatDocumentsAsString} from "langchain/util/document";
+import {chat, ChatHandler} from "./utils/chat";
+import {BaseMessage, AIMessage, HumanMessage} from "@langchain/core/messages";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const prompt = ChatPromptTemplate.fromMessages([
+    [
+        "system",
+        `You are an assistant for question-answering tasks. Your name is ${process.env['ASSISTANT_NAME'] || 'Assistant'}. 
+        Here are the rules for answering the question:
+        1) Answer only in ${process.env['LANGUAGE'] || 'english'} when it is possible unless otherwise specified by the user.
+        2) The answer must be relevant to the question asked and as complete as possible.
+        3) Be very careful, the question you have to answer is not in the context or even in the chat history, it is only the last question asked by the human.
+        4) If you don't know the answer, just say that you don't know.
+        5) The provided context may include both relevant and irrelevant information. You should focus primarily on what is clearly related to the current question.
+        6) If the question refers to a specific topic (e.g. a project, a person, a product, a responsibility), include all the context that **is reasonably associated with that topic**, even if it comes in multiple parts or uses slightly different wording.
+        7) Ignore unrelated topics, other projects, or unrelated roles—even if they appear in the context.
+        8) Do not infer or assume anything that is not supported by the context.
+        9) Your answers should be concise, accurate, and focused.
+
+        Use the following pieces of retrieved context to answer the question.
+        Start Context: {context}
+        End Context.
+     `],
+    new MessagesPlaceholder("chat_history"),
+    ["human", "{question}"]
+]);
+
+const llm = new Ollama(
+    {
+        model: process.env['LLM_MODEL'] || 'llama3',
+        temperature: 0.2,
+    }
+);
+
+const outputParser = new StringOutputParser();
+
+const retriever = await createRetriever();
+
+const retrievalChain = RunnableSequence.from([
+    (input) => input.question,
+    retriever,
+    formatDocumentsAsString
+]);
+
+const generationChain = RunnableSequence.from([
+    {
+        question: (input) => input.question,
+        context: retrievalChain,
+        chat_history: (input) => input.chat_history
+    },
+    prompt,
+    llm,
+    outputParser
+]);
+
+/*
+const qcSystemPrompt = `Given a chat history and the latest user question which might reference context in the chat history,
+formulate a standalone question which can be understood without the chat history. Do NOT answer question, just reformulate it
+if needed and otherwise return it as is.`;
+
+const qcPrompt = ChatPromptTemplate.fromMessages([
+    ["system", qcSystemPrompt],
+    new MessagesPlaceholder("chat_history"),
+    ["human", "{question}"]
+]);
+
+const qcChain = RunnableSequence.from([
+    qcPrompt,
+    llm,
+    outputParser
+]);
+*/
+const chatHistory: BaseMessage[] = [];
+
+const chatHandler: ChatHandler = async (question: string) => {
+
+    return {
+        answer: generationChain.stream({
+            question: question,
+            chat_history: chatHistory,
+        }),
+        answerCallBack: async (answerText: string) => {
+            chatHistory.push(new HumanMessage(question));
+            chatHistory.push(new AIMessage(answerText));
+        }
+    }
+}
+
+chat(chatHandler);
